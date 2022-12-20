3f36128a73cf7249e84cfca9d73e92a0b1d45e73
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-12-20 13:16:28 +0900

    basic

--- src/main/java/love/chihuyu/Plugin.kt
+++ src/main/java/love/chihuyu/Plugin.kt
@@ -1,7 +1,12 @@
 package love.chihuyu
 
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.java.JavaPlugin
+import java.time.Instant
+import java.util.*
 
 class Plugin : JavaPlugin(), Listener {
 
@@ -15,5 +20,14 @@ class Plugin : JavaPlugin(), Listener {
 
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
+    }
+
+    @EventHandler
+    fun onHorn(e: PlayerInteractEvent) {
+        val item = e.item ?: return
+        val player = e.player
+
+        if (item.type != Material.GOAT_HORN) return
+        player.banPlayer("Used goat horn (1hour)", Date.from(Instant.ofEpochSecond(Instant.now().epochSecond + (60 * 60))))
     }
 }
